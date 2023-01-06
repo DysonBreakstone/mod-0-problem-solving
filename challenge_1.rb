3981a17e39774be83baaf34ccece13243c144d3b
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-01-06 12:28:03 -0700

    Add challenge_1.rb

---
+++ challenge_1.rb
@@ -0,0 +1,19 @@
+# # Given an array of string,s return only the strings that have exactly 4 characters.
+
+# names = ["Jeff", "Bob", "Jim", "Dyson"]
+
+# names.each do |name|
+#     puts name if name.length == 4
+# end
+
+
+# Given an array of strings with mix of uppercase and lowercase letters
+# Print every word in all lowercase letters
+
+
+random_characters = ["jhJGGIUjkhuknF", "JhYgTfRcNjhJi", "IUHgyfybUHiuHUGJH", "IuiHIUhUKhKbUGrCrB"]
+
+random_characters.each do |string|
+    puts string.downcase
+end
+
